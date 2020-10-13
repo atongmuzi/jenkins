fa899179f9842e011c637e70833bffb5b59e4a1d
atongmuzi <jazx505@163.com>
2020-10-13 15:41:39 +0800

    test sample

---
+++ src/test/java/atong/springboot/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package atong.springboot.jenkins;
+
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+ public  class JenkinsApplicationTests extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("hahaha");
+    }
+    @Test
+    public  void sample(){
+        System.out.println("lalala");
+    }
+
+
+}
