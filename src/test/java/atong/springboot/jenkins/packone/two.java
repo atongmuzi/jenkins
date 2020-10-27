9b05a61b5ca19afd73dfb8cb368f3a9116dd01d4
atongmuzi <jazx505@163.com>
2020-10-27 15:06:11 +0800

    commit two

--- src/test/java/atong/springboot/jenkins/packone/two.java
+++ src/test/java/atong/springboot/jenkins/packone/two.java
@@ -12,30 +12,30 @@ import org.testng.annotations.Test;
 @SpringBootTest
  public  class two extends AbstractTestNGSpringContextTests {
 
-//    @Test
-//    public void oneContextLoads() {
-//        System.out.println("hahaha2");
-//    }
-//    @Test
-//    public  void twoSample(){
-//        System.out.println("lalala2");
-//    }
-//    @Test
-//    public void twoSample1(){
-//        System.out.println("hehehe2");
-//    }
-//
-//    @Test
-//    @Feature("atong test")
-//    @Story("story1")
-//    @Description("该案例是为了测试通过枚举类型引入数据")
-//    public void twoSample2(){
-//        System.out.println("xixixi2");
-//    }
-//    @Test
-//    @Description("该案例是为了测试通过枚举类型引入数据")
-//    public void twoSample3(){
-//        System.out.println("xixixi2");
-//    }
+    @Test
+    public void oneContextLoads() {
+        System.out.println("hahaha2");
+    }
+    @Test
+    public  void twoSample(){
+        System.out.println("lalala2");
+    }
+    @Test
+    public void twoSample1(){
+        System.out.println("hehehe2");
+    }
+
+    @Test
+    @Feature("atong test")
+    @Story("story1")
+    @Description("该案例是为了测试通过枚举类型引入数据")
+    public void twoSample2(){
+        System.out.println("xixixi2");
+    }
+    @Test
+    @Description("该案例是为了测试通过枚举类型引入数据")
+    public void twoSample3(){
+        System.out.println("xixixi2");
+    }
 
 }
