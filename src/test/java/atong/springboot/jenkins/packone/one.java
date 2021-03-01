cce633af987a6375e8db400540dd8be91a7de069
atongmuzi <jazx505@163.com>
2021-03-01 15:17:45 +0800

    add assert

--- src/test/java/atong/springboot/jenkins/packone/one.java
+++ src/test/java/atong/springboot/jenkins/packone/one.java
@@ -5,14 +5,17 @@ package atong.springboot.jenkins.packone;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.Assert.*;
+import org.slf4j.Logger;
 
 @SpringBootTest
  public  class one extends AbstractTestNGSpringContextTests {
+    private static final Logger log = LoggerFactory.getLogger(one.class);
 
     @Test
     public void oneContextLoads() {
@@ -43,7 +46,12 @@ import org.testng.Assert.*;
     @Test
     public void oneSample4(){
        int i = 8;
-        Assert.assertEquals(i,10);
+       if (i==7){
+           log.info("success");
+       }else {
+           Assert.fail("hahaha fail");
+       }
+
     }
 
 
